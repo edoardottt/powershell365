38b5ce7afc9789a1aa81974dbeba89f465189e9f
edoardottt <edordott@gmail.com>
2021-08-31 16:04:37 +0200

    update day 6

---
+++ scripts/day006/day006.ps1
@@ -0,0 +1,16 @@
+# Day 6 - PowerShell365
+# Use the Get-Help core cmdlet to invoke
+# a built-in help system.
+# https://github.com/edoardottt/powershell365
+
+
+# These examples display basic help information about the Format-Table cmdlet.
+Get-Help Format-Table
+Get-Help -Name Format-Table
+Format-Table -?
+
+# These examples display more detailed help information about the Format-Table cmdlet.
+Get-Help Format-Table -Detailed
+Get-Help Format-Table -Full
+
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/get-help?view=powershell-7.1
